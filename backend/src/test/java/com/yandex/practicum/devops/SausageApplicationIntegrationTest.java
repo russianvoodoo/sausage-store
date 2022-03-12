f4c8e67c7868f38e800aac3eb13c3078d8372e98
russianvoodoo4 <russianvoodoo4@yandex.ru>
2022-03-12 21:10:26 +0300

    fixed test

--- backend/src/test/java/com/yandex/practicum/devops/SausageApplicationIntegrationTest.java
+++ backend/src/test/java/com/yandex/practicum/devops/SausageApplicationIntegrationTest.java
@@ -61,7 +61,7 @@ public class SausageApplicationIntegrationTest {
         assertThat(products, hasItem(hasProperty("name", is("Молочная"))));
         assertThat(products, hasItem(hasProperty("name", is("Нюренбергская"))));
         assertThat(products, hasItem(hasProperty("name", is("Мюнхенская"))));
-        assertThat(products, hasItem(hasProperty("name", is("Русская"))));
+        assertThat(products, hasItem(hasProperty("name", is("Еврейская"))));
     }
 
     @Test
@@ -90,7 +90,7 @@ public class SausageApplicationIntegrationTest {
     private OrderController.OrderForm prepareOrderForm() {
         OrderController.OrderForm orderForm = new OrderController.OrderForm();
         OrderProductDto productDto = new OrderProductDto();
-        productDto.setProduct(new Product(1L, "Русская", 300.00, "http://placehold.it/200x100"));
+        productDto.setProduct(new Product(6L, "Еврейская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
         productDto.setQuantity(2);
         orderForm.setProductOrders(Collections.singletonList(productDto));
 

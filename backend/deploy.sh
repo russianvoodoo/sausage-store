1c0cd26980f65b157b539c0b09951cc385fa96b8
Илья Задохин <russianvoodoo4@yandex.ru>
2022-04-21 22:09:27 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,5 +3,5 @@
 set -xe
 #скачиваем артефакт
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_BACKEND_REPO_URL}com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-#запускаем java-приложение в фоне
-nohup "java -jar sausage-store-${VERSION}.jar --server.port=9090 &"
+#Пытаемся запустить приложение фоном в новой сессии
+setsid nohup java -jar sausage-store-${VERSION}.jar --server.port=9090 > nohup.out & 

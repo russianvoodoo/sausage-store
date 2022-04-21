2b275c179e5549a233333e86ceec76371c85ab86
Илья Задохин <russianvoodoo4@yandex.ru>
2022-04-21 22:21:30 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-#чтобы скрипт завершался, если есть ошибки
+#Если свалится одна из команд, рухнет и весь скрипт
 set -xe
-#скачиваем артефакт
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo rm -f /home/jarservice/sausage-store.jar||true
+#Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_BACKEND_REPO_URL}com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-#Пытаемся запустить приложение фоном в новой сессии
-setsid nohup java -jar sausage-store-${VERSION}.jar --server.port=9090 > nohup.out & 
+sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
+#Обновляем конфиг systemd с помощью рестарта
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store

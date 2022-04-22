d63791802eca399a97d415d02c759836d7373dbf
Илья Задохин <russianvoodoo4@yandex.ru>
2022-04-22 15:27:26 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,6 +7,6 @@ sudo rm -rf /var/www/dist/frontend/*||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-front.tar.gz ${NEXUS_FRONTEND_REPO_URL}sausage-store-front/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar xf ./sausage-store-front.tar.gz -C /home/front-user/sausage-front/
-sudo cp -r /home/front-user/sausage-front/sausage-store-${VERSION}/public_html /var/www/dist/frontend/||true
+sudo cp -r /home/front-user/sausage-front/sausage-store-${VERSION}/public_html /var/www-data/dist/frontend/||true
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-frontend.service

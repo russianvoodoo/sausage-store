20576dfb685b849c4b94ffac9c58072cc691f2cd
Илья Задохин <russianvoodoo4@yandex.ru>
2022-04-22 14:26:39 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,10 +3,10 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
-sudo rm -f /home/jarservice/sausage-store.jar||true
+sudo rm -f /var/www/dist/frontend/*||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-front.tar.gz ${NEXUS_FRONTEND_REPO_URL}'sausage-store-front/sausage-store/'${VERSION}'/sausage-store-'${VERSION}'.tar.gz
 sudo tar -xf ./sausage-store-front.tar.gz
-sudo cp sausage-store/public_html /home/front-user/||true #"jar||true" говорит, если команда обвалится — продолжай
+sudo cp sausage-store/public_html /var/www/dist/frontend/||true
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-frontend.service

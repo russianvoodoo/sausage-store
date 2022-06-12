6e705896f94d9406c824ed9e3f8313bc25d02c42
Илья Задохин <russianvoodoo4@yandex.ru>
2022-06-12 19:24:07 +0000

    Update backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -8,6 +8,7 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
 docker network create -d bridge sausage_network || true
+docker login gitlab.praktikum-services.ru:5050 -u ${GITLAB_USER} -p ${GITLAB_PASS}
 docker pull gitlab.praktikum-services.ru:5050/russianvoodoo4/sausage-store/sausage-backend:latest
 docker stop backend || true
 docker rm backend || true
@@ -17,4 +18,4 @@ docker run -d --name backend \
     --restart always \
     --pull always \
     --env-file .env \
-    <реестр Gitlab Registry>/sausage-store/sausage-backend:latest
+    gitlab.praktikum-services.ru:5050/russianvoodoo4/sausage-store/sausage-backend:latest

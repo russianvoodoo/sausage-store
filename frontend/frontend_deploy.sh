f6f0a57814508d6c5f7c6237ac6120710e468fe1
Илья Задохин <russianvoodoo4@yandex.ru>
2022-06-12 20:16:03 +0000

    Update frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set +e
-cat > .env <<EOF
-EOF
 docker network create -d bridge sausage_network || true
 docker login gitlab.praktikum-services.ru:5050 -u ${GITLAB_USER} -p ${GITLAB_PASS}
 docker pull gitlab.praktikum-services.ru:5050/russianvoodoo4/sausage-store/sausage-frontend:latest
@@ -12,5 +10,4 @@ docker run -d --name frontend -p 8080:80 \
     --network=sausage_network \
     --restart always \
     --pull always \
-    --env-file .env \
     gitlab.praktikum-services.ru:5050/russianvoodoo4/sausage-store/sausage-frontend:latest

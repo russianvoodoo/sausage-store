5a9c4bc3e2e201875fb1078267925f521f51d092
russianvoodoo4 <russianvoodoo4@yandex.ru>
2022-06-24 00:25:05 +0300

    added CISHA and sonar backend report fixed deploy shell fixed

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -3,6 +3,6 @@ set +e
 VAULT_TOKEN=${VAULT_TOKEN}
 VAULT_HOST=${VAULT_HOST}
 VAULT_PORT=${VAULT_PORT}
-EOF
+CI_COMMIT_SHA=${CI_COMMIT_SHA}
 docker login gitlab.praktikum-services.ru:5050 -u ${GITLAB_USER} -p ${GITLAB_PASS}
 docker-compose up -d backend
